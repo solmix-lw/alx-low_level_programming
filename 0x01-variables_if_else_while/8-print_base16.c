a537988b1b86ae811c4d9b4e2f5c83b77788d031
solmix-lw <solmix88@gmail.com>
2022-04-11 11:26:20 +0000

    HOt fix for task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,14 +8,16 @@
  */
 int main(void)
 {
-	int d;
-	char low;
-
-	for (d = '0'; d <= '9'; d++)
-		putchar(d);
-	for (low = 'a'; low <= 'f'; low++)
-		putchar(low);
-		putchar('\n');
-
-	return (0);
+int d;
+char low;
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+}
+for (low = 'a'; low <= 'f'; low++)
+{
+putchar(low);
+}
+putchar('\n');
+return (0);
 }
