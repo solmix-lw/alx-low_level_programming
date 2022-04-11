21fc592766060a4f28a63cbe0962ceb9ca905f5f
solmix-lw <solmix88@gmail.com>
2022-04-11 11:23:07 +0000

    Hot fix for task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-	char low;
-
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
-		putchar('\n');
-
-	return (0);
+char low;
+for (low = 'z'; low >= 'a'; low--)
+{
+putchar(low);
+}
+putchar('\n');
+return (0);
 }
