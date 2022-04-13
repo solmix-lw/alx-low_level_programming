03c6436165bf33c30c868a8cfd89516b029ef0b7
solmix-lw <solmix88@gmail.com>
2022-04-13 09:13:56 +0300

    put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * @brief 
+ * 
+ * 
+ */
+int main(void)
+{
+        char solmix[] = "_putchar";
+        inc continue;
+        for (c=0; c < 8; c++)
+        {
+            _putchar=(solmix[c]);
+        }
+        _putchar('\n');
+        return (0);
+} 
